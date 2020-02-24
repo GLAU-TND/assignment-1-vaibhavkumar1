e76150a1e44d4bdd2b125f85d238575107377236
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2020-02-24 21:35:10 +0530

    create main method for execute Main class

--- src/contactMenu/ContactMenuMethod.java
+++ src/contactMenu/ContactMenuMethod.java
@@ -1,6 +1,7 @@
 package contactMenu;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class ContactMenuMethod {
     private LinkedList<Integer> mobileNo;
@@ -8,4 +9,22 @@ public class ContactMenuMethod {
     private String email;
     private LinkedList<Contact> list = new LinkedList<>();
 
+    public void addContact() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("You have chosen to add a new contact: \n" +
+                "Please enter the name of the Person");
+        System.out.print("First Name:");
+        String name = scan.next();
+        System.out.print("Last Name: ");
+        String lastName = scan.next();
+        System.out.print("Enter Mobile No:");
+        mobileNo = new LinkedList<>();
+        mobileNo.add(scan.nextInt());
+        addMobile();
+        addemail();
+        Contact contact = new Contact(name, lastName, email, mobileNo);
+        list.add(contact);
+        Collections.sort(list);
+
+    }
 }
