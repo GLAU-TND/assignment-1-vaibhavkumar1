f122c9823c3ce1c89dd3eadc54a7b7fe89e6cfe2
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2020-02-24 21:22:51 +0530

    create field for Contact class

--- src/contactMenu/Contact.java
+++ src/contactMenu/Contact.java
@@ -1,6 +1,14 @@
 package contactMenu;
 
+import java.util.LinkedList;
+
 public class Contact implements Comparable<Contact> {
+
+    private LinkedList<Integer> mobileNo;
+    private String name;
+    private String lastName;
+    private Contact contact;
+    private String email;
 
     @Override
     public int compareTo(Contact o) {
