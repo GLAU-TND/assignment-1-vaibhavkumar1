847b856c79b3b038ede7d70aa25029c31a6bb5fc
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2020-02-24 21:21:41 +0530

    we implement comparable interface

--- src/contactMenu/Contact.java
+++ src/contactMenu/Contact.java
@@ -1,5 +1,9 @@
 package contactMenu;
 
-public class Contact {
+public class Contact implements Comparable<Contact> {
 
+    @Override
+    public int compareTo(Contact o) {
+        return 0;
+    }
 }
