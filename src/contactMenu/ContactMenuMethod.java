3bbd3bad8de5d9e54ab78cdd4b884f959766e22c
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2020-02-24 21:28:43 +0530

    create field email,contact,mobileno,contactlist

--- src/contactMenu/ContactMenuMethod.java
+++ src/contactMenu/ContactMenuMethod.java
@@ -1,5 +1,11 @@
 package contactMenu;
 
+import java.util.LinkedList;
+
 public class ContactMenuMethod {
+    private LinkedList<Integer> mobileNo;
+    private Contact contact;
+    private String email;
+    private LinkedList<Contact> list = new LinkedList<>();
 
 }
